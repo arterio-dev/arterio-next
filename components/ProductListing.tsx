c9c4a04b247e6fdd306292e5c8499fe5efbc86c8
Gabriel Maia <maiasena.gabriel@matematica.ufrj.br>
2026-05-11 18:14:22 -0300

    Update ProductListing.tsx

--- components/ProductListing.tsx
+++ components/ProductListing.tsx
@@ -64,7 +64,7 @@ export function ProductListing({ onNotifyMe, selectedCategoryId, selectedCategor
             )}
           </div>
 
-          <div className="grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:grid-cols-3 sm:gap-x-8 sm:gap-y-16">
+          <div className="grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-4 lg:grid-cols-5 sm:gap-x-8 sm:gap-y-16">
             {currentProducts.map((product) => (
               <ProductCard 
                 key={product.id} 
